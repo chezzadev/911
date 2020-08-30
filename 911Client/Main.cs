7b607a9bcb3631ee903a3d400b69f0b3a62c7a28
chezza <bencherrington28@gmail.com>
2020-08-30 18:22:23 +0100

    Add project files.

---
+++ 911Client/Main.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using static CitizenFX.Core.UI.Screen;
+using static CitizenFX.Core.Native.API;
+
+namespace _911
+{
+    public class Main : BaseScript
+    {
+        bool callActive = false;
+        public Main()
+        {
+            EventHandlers.Add("911:AddBlipToMap", new Action<Vector3, string, int>(AddBlipToMap));
+
+            RegisterCommand("911", new Action<int, List<dynamic>, string>((source, args, rawCommand) =>
+            {
+                if (!callActive)
+                {
+                    string name = Game.Player.Name;
+                    string msg = string.Join(" ", args);
+                    Vector3 coords = GetEntityCoords(Game.PlayerPed.Handle, true);
+                    string location = World.GetStreetName(coords);
+                    TriggerServerEvent("911:SendMessage", coords, name, location, msg);
+                } else
+                {
+                    TriggerEvent("chatMessage", "You have a call currently active please wait.");
+                }
+            }), false);
+        }
+
+        public async void AddBlipToMap(Vector3 coords, string name, int timeout) 
+        {
+            callActive = true;
+            var blip = AddBlipForCoord(coords.X, coords.Y, coords.Z);
+            SetBlipSprite(blip, 162);
+            SetBlipColour(blip, 38);
+            BeginTextCommandSetBlipName("STRING");
+            AddTextComponentString("911 Report - " + name);
+            EndTextCommandSetBlipName(blip);
+            await Delay(timeout);
+            RemoveBlip(ref blip);
+            callActive = false;
+        }
+    }
+}
---
+++ 911Server/Main.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
+
+namespace _911Server
+{
+    public class Main : BaseScript
+    {
+        bool displayLocation = false;
+        int timeout = 0;
+        bool discord = false;
+        string webhookurl = null;
+
+        public Main()
+        {
+            LoadConfig();
+            EventHandlers.Add("911:SendMessage", new Action<Player, Vector3, string, string, string>(SendMessage));
+        }
+
+        public void LoadConfig()
+        {
+            string config = null;
+            try
+            {
+                config = LoadResourceFile(GetCurrentResourceName(), "config.ini");
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine("Error occurred while loading the config file.");
+            }
+            Config cfg = new Config(config);
+            displayLocation = cfg.Get("location") == "true";
+            string timeoutString = cfg.Get("blipTimeout", "30");
+            if (int.TryParse(timeoutString, out int val))
+            {
+                timeout = val * 1000;
+            }
+            discord = cfg.Get("discord") == "true";
+            if (discord)
+            {
+                webhookurl = cfg.Get("webhookurl").Replace('"', ' ').Trim();
+            }
+        }
+
+        public void SendMessage([FromSource] Player source, Vector3 coords, string name, string location, string msg)
+        {
+            if (displayLocation)
+            {
+                TriggerClientEvent("chatMessage", "^*^5911 ^r^7- " + name + " ^*^5[^r" + location + "^*^5]^r: " + msg);
+                TriggerClientEvent("911:AddBlipToMap", coords, name, timeout);
+                if (discord)
+                {
+                    TriggerEvent("911:SendDiscordMessage", name, msg, location, webhookurl);
+                }
+            } else
+            {
+                TriggerClientEvent("chatMessage", "^*^5911 ^r^7- " + name + ": " + msg);
+                if (discord)
+                {
+                    TriggerEvent("911:SendDiscordMessage", name, msg, "Disabled", webhookurl);
+                }
+            }
+        }
+    }
+}
